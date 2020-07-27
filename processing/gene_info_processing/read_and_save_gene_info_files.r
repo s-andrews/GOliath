2b0f9c1f3f0d6de4ba921589ef0044942a3b4118
Laura Biggins <bigginsl@babraham.ac.uk>
2020-07-27 11:21:20 +0100

    fixed bug where gene names weren't converted to upper case in mouse and human genfo files which messed up the screening plots

--- processing/gene_info_processing/read_and_save_gene_info_files.r
+++ processing/gene_info_processing/read_and_save_gene_info_files.r
@@ -1,9 +1,12 @@
 # human
 gene_info_file_location <- here::here("gene_info_data/Homo sapiens/","GRCh38.80_gene_info.txt")
 human_genfo <- data.table::fread(gene_info_file_location, select = c(1:5,7:11), stringsAsFactors = TRUE, data.table = FALSE)
-save(human_genfo, file = "human_genfo.rda")
+human_genfo <- mutate(human_genfo, gene_name = toupper(gene_name))
+save(human_genfo, file = "processing/gene_info_processing/human_genfo.rda")
 
 # mouse
 gene_info_file_location <- here::here("gene_info_data/Mus musculus","GRCm38.80_gene_info.txt")
 mouse_genfo <- data.table::fread(gene_info_file_location, select = c(1:5,7:11), stringsAsFactors = TRUE, data.table = FALSE)
-save(mouse_genfo, file = "mouse_genfo.rda")
+mouse_genfo <- mutate(mouse_genfo, gene_name = toupper(gene_name))
+
+save(mouse_genfo, file = "processing/gene_info_processing/mouse_genfo.rda")
