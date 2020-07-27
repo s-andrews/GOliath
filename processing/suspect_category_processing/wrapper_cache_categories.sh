85888cb0e1c5d264653cf3de7e847db8f32fe603
laurabiggins <laurabiggins10@gmail.com>
2020-07-27 10:58:13 +0100

    adding yeast genome

--- processing/suspect_category_processing/wrapper_cache_categories.sh
+++ processing/suspect_category_processing/wrapper_cache_categories.sh
@@ -2,3 +2,4 @@
 # if it doesn't work properly, check the line endings of the category files - this needs sorting in R to print unix ones
 ./cache_categories_human.sh
 ./cache_categories_mouse.sh
+./cache_categories_yeast.sh
