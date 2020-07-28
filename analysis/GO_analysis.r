7dcef579c2e371c3f62c2d638080836ad165f272
laurabiggins <laurabiggins10@gmail.com>
2020-07-28 14:49:10 +0100

    cleaning up analysis code

--- analysis/GO_analysis.r
+++ analysis/GO_analysis.r
@@ -2,15 +2,13 @@ suppressMessages(library(data.table))
 suppressMessages(library(tidyverse))
 suppressMessages(library(here))
 suppressMessages(library(runGOA))
-suppressMessages(library("magrittr"))
-suppressMessages(library("dplyr"))
-suppressMessages(library("ggplot2"))
+suppressMessages(library(magrittr))
 
 # these functions will be packaged up so that the package can just be loaded,
 # but for now we'll just source the files
 #source(here::here("analysis","GOliath_functions.r"))
 #source(here::here("analysis","overrepresentation_test.r"))
-source(here::here("analysis","plots.R"))
+#source(here::here("analysis","plots.R")) !!get rid of these files!!
 source(here::here("analysis","utilities.R"))
 
 # pass in the job folder as the argument
@@ -19,6 +17,9 @@ cmdArgs <- commandArgs()
 folder.path <- cmdArgs[6]
 
 setwd(folder.path)
+
+#!! early on we should check how many query and bg genes are in the genfo and go categories files
+#!! if there are no results - report this properly, check whether plots should be produced
 
 # import the config file
 config.info <- read.delim("config.txt", header = FALSE, row.names = 1)
@@ -72,55 +73,38 @@ print(head(query_genes))
 print(head(bg_genes))
 species_common <- species_tail
 
-#if (grepl(pattern = "Homo_Sapiens", species)) {
-#    species_common <- "human"
-#} else if (grepl(pattern = "Mus musculus", species)) {
-#    species_common <- "mouse"
-#} else {
-#    stop("Couldn't match species")
-#}
-
+#===============================================
+# load the go categories and the gene info file
+#===============================================
 
 if (species_common == "human") {
+
     go.categories <- human_categories
-} else if (species_common == "mouse") {
-    go.categories <- mouse_categories
-} else if (species_common == "yeast") {
-    load(here::here("godata/yeast/yeast_categories.rda"))
-    go.categories <- yeast_categories
-    rm(yeast_categories)
-}  else {
-    stop("Shouldn't have got to here, something's gone wrong")
-}
-
-#===========================
-# import the gene info file
-#===========================
-# this needs sorting properly 
-if (species_common == "human") {
-
+    
     load(here::here("processing/gene_info_processing/human_genfo.rda"))
     all_gene_info <- human_genfo
     rm(human_genfo)
     
 } else if (species_common == "mouse") {
-  
+
+    go.categories <- mouse_categories
+    
     load(here::here("processing/gene_info_processing/mouse_genfo.rda"))
     all_gene_info <- mouse_genfo
     rm(mouse_genfo)
     
 } else if (species_common == "yeast") {
-  
-  load(here::here("processing/gene_info_processing/yeast_genfo.rda"))
-  all_gene_info <- yeast_genfo
-  rm(yeast_genfo)
-  
-} else {
-    print("Couldn't find gene info file")
+    load(here::here("godata/yeast/yeast_categories.rda"))
+    go.categories <- yeast_categories
+    rm(yeast_categories)
+    
+    load(here::here("processing/gene_info_processing/yeast_genfo.rda"))
+    all_gene_info <- yeast_genfo
+    rm(yeast_genfo)
+    
+}  else {
+    stop("Shouldn't have got to here, something's gone wrong")
 }
-
-# make sure gene names are upper case - they should already be in the genfo file as part of the pre-processing, don't duplicate
-#all_gene_info[,"gene_name"] <- toupper(all_gene_info[,"gene_name"])
 
 print("Sorting background genes")
 print(Sys.time())
@@ -130,8 +114,6 @@ print(Sys.time())
 #=======================
 # set the background as all the genes present in the gmt file
 # this needs to be a more robust check...
-ifelse((length(bg_genes) == 0), bg_genes <- unique(unlist(go.categories)), bg_genes <- remove_duplicates(bg_genes))
-
 if(length(bg_genes) == 0) {
   bg_genes <- unique(unlist(go.categories))
 
@@ -145,16 +127,8 @@ if(length(bg_genes) == 0) {
 #====================
 query_genes <- remove_duplicates(query_genes)
 
-# check whether all the query genes are in the background genes 
-# we add the query genes to the background genes so this isn't needed
+# we add the query genes to the background genes so we're no longer checking that the query genes are in the bg genes
 query_filt <- query_genes
-#if (sum(!query_genes %in% bg_genes > 0)) {
-#    print("not all query genes found in background genes")
-#    print(query_genes[!query_genes %in% bg_genes])
-#    
-#    query_filt <- query_genes[query_genes %in% bg_genes]
-#}
-	
 
 print("About to run GO analysis")
 print(Sys.time())
@@ -162,7 +136,8 @@ print(Sys.time())
 #===========================
 # The GO analysis
 #=============================
-go_results <- runGOA::overrep_test(go.categories, query_filt, bg_genes, min_genes_in_category = min_category_size, max_genes_in_category = max_category_size)
+go_results <- runGOA::overrep_test(go.categories, query_filt, bg_genes, 
+    min_genes_in_category = min_category_size, max_genes_in_category = max_category_size)
 
 print("Got the GO results")
 print(Sys.time())
@@ -172,8 +147,7 @@ if(is.null(go_results)){
 	write.table(go_results, "GO_analysis_results.txt", quote = FALSE, sep = "\t")
     sink("summary_stats.txt")
     print("no categories flagged")
-    sink()
-    
+    sink()    
     
 } else {	
 
@@ -181,30 +155,11 @@ if(is.null(go_results)){
 	# check against suspect categories
 	#==================================
 	
-    if (species_common == "human") {
-        suspects <- read.delim(here::here("suspect_GO_categories/human","suspect_categories.txt"))
-        print("read the human suspect file")
-    } else if (species_common == "mouse") {
-        suspects <- read.delim(here::here("suspect_GO_categories/mouse","suspect_categories.txt"))
-        print("read the mouse suspect file")
-    } else if (species_common == "yeast") {
-      suspects <- read.delim(here::here("suspect_GO_categories/yeast","suspect_categories.txt"))
-      print("read the yeast suspect file")
-    } else {
-        stop("Shouldn't have got to here, something's gone wrong with finding the suspect categories")
-    }
-
+    suspect_file <- paste0("suspect_GO_categories/", species_common, "/suspect_categories.txt")
+    suspects <- read.delim(here::here(suspect_file))
 	print("read the suspect file")
 
 	sig_categories <- rownames(go_results)
-
-	# it doesn't work just taking the last category as the wikipathways have the organism as the 4th field.
-	# get_id <- function(description){
-		# ifelse(grepl(x= description, pattern = "%GO:"),
-			# strsplit(description, split = "%", fixed = TRUE)[[1]][3], 
-			# description
-		# )
-	# }
 
 	get_id <- function(description){
 		if(grepl(x = description, pattern = "%GO:")) {
@@ -240,36 +195,31 @@ if(is.null(go_results)){
 	categories_not_flagged  <- sum(flag_descriptions == "none found")
 	categories_flagged      <- sum(flag_descriptions != "none found")
 
-  print("printing the size of categories flagged and unflagged")
-	print(total_sig_categories)  
-	print(categories_not_flagged)
-	print(categories_flagged)
+    print("printing the size of categories flagged and unflagged")
+    print(total_sig_categories)  
+    print(categories_not_flagged)
+    print(categories_flagged)
 
-	size_of_bias_categories <- suspects %>%
-		count(bias_source) %>%
-		arrange(desc(n))
-		
-	#number of GO categories flagged with each bias
-	size_of_bias_categories$number_flagged <- sapply(size_of_bias_categories$bias_source, function(y) {
-		grep_text <- paste0("\\b", y, "\\b")
-		length(grep(x = flag_descriptions, pattern = grep_text))
-	})
+    size_of_bias_categories <- suspects %>%
+        count(bias_source) %>%
+        arrange(desc(n))
+        
+    #number of GO categories flagged with each bias
+    size_of_bias_categories$number_flagged <- sapply(size_of_bias_categories$bias_source, function(y) {
+        grep_text <- paste0("\\b", y, "\\b")
+        length(grep(x = flag_descriptions, pattern = grep_text))
+    })
 
   df_summary <- data.frame("number of significant categories identified" = total_sig_categories,
              "number of categories flagged as potential biases" = categories_flagged,
              "number of categories not flagged" = categories_not_flagged)
 
 
-	sink("summary_stats.txt")
+  sink("summary_stats.txt")
 
-# The df_summary would probably be better as some sprintf statements
-
-  # print(paste0("number of significant categories identified = ", total_sig_categories))
-  # print(paste0("number of categories flagged as potential biases = ", categories_flagged))       
-  # print(paste0("number of categories not flagged = ", categories_not_flagged))
   print(as.data.frame(size_of_bias_categories))
 
-	sink()
+  sink()
 
   print("I'd like this df info")
   df_summary
@@ -280,48 +230,6 @@ if(is.null(go_results)){
 print("Checked against suspect categories")
 print(Sys.time())
 
-
-#=================
-# screening plots
-#=================
-
-#if (!is.null(all_gene_info)) {
-#    
-#    chromosomes <- list(
-#        mouse = c(1:19,"MT","X","Y"),
-#        human = c(1:22,"MT","X","Y"),
-#        rat = c(1:20,"MT","X","Y"),  
-#        worm = c("I","II","III","IV","MtDNA","V","X"),
-#        zebrafish = c(1:25,"MT")
-#    )
-#    
-#    # clean up the gene names
-#    #   all_gene_info[,"gene_name"] <- toupper(clean_text(all_gene_info[,"gene_name"]))
-#    
-#    print(head(all_gene_info))
-#    
-#    #=============================================
-#    # remove any genes not in the background list
-#    #=============================================
-#    print("number of background genes =")
-#    print(length(bg_genes))
-#    print(head(bg_genes))
-#    gene_info <- all_gene_info[all_gene_info[,"gene_name"] %in% bg_genes,]
-#    
-#    print("number of genes in gene info file that matched the background genes = ")
-#    print(nrow(gene_info))
-#    
-#    # we probably have duplicates in the gene info file but this shouldn't be a problem if we use ensembl ids
-#    
-#    # add a TRUE/FALSE column for whether the gene is a query gene
-#    gene_info$query <- gene_info[,"gene_name"] %in% query_filt
-#    
-#}
-
-print("screening plot filtering done")
-print(Sys.time())
-
-
 #============
 # The plots
 #============
@@ -329,11 +237,6 @@ print(Sys.time())
 #=============
 # The GC plot 
 #=============
-
-#print(head(gene_info))
-
-#query_GC <- get_GC(query_filt, gene_info)
-#bg_GC    <- get_GC(bg_genes, gene_info)
 
 query_GC <- all_gene_info %>%
   filter(gene_name %in% query_filt) %>%
@@ -371,8 +274,6 @@ print("done GC plot")
 #=============
 # length plot
 #=============
-#query_lengths <- get_lengths(query_filt, gene_info)
-#bg_lengths    <- get_lengths(bg_genes, gene_info)
 
 query_lengths <- all_gene_info %>%
   filter(gene_name %in% query_filt) %>%
@@ -381,7 +282,6 @@ query_lengths <- all_gene_info %>%
 bg_lengths <- all_gene_info %>%
   filter(gene_name %in% bg_genes) %>%
   pull(length)
-
 
 length_data <- tibble(gene_length = query_lengths, type = "query") %>%
   bind_rows(tibble(gene_length = bg_lengths, type = "background"))
@@ -410,9 +310,6 @@ dev.off()
 # chr plot
 #=============
 
-#query_chr <- get_chromosomes(query_filt, gene_info)
-#bg_chr    <- get_chromosomes(bg_genes, gene_info)
-
 query_chr <- all_gene_info %>%
   filter(gene_name %in% query_filt) %>%
   pull(chromosome)
@@ -420,8 +317,6 @@ query_chr <- all_gene_info %>%
 bg_chr <- all_gene_info %>%
   filter(gene_name %in% bg_genes) %>%
   pull(chromosome)
-
-#chrs <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,"X","Y")
 
 chromosomes <- list(
   mouse = c(1:19,"MT","X","Y"),
